dae99ce3a3d29fb29099b69354395f92afcb157d
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2022-03-24 15:56:07 -0300

    Add project files.

---
+++ Controllers/VendaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Domain;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class VendaController : ControllerBase
+    {
+        private IVenda _iVenda;
+
+        public VendaController(IVenda ivenda)
+        {
+            _iVenda = ivenda;
+        }
+
+        //Requisicao veio do navegador
+        [HttpGet]
+        public IActionResult Finalizar(string idProduto)
+        {
+            try
+            {
+                _iVenda.OferecerCartaoDeCredito();
+                _iVenda.Finalizar(idProduto);
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Cancelar(string idProduto)
+        {
+            try
+            {
+                _iVenda.Cancelar(idProduto);
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
